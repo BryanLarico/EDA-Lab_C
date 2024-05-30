ece22bf77546183976cfcc13ad97210fe6550e88
BryanLarico <blaricor@unsa.edu.pe>
2024-05-29 21:27:36 -0500

    Ejercicio1 de los resueltos en clase

---
+++ Lab03/EjerciciosResueltos/Ejercicio1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Ejercicio1 {
+	public static void main(String[] args) {
+		int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
+
+		System.out.println(sumaArrayDyV(0, array.length-1, array));
+	}
+	public static int sumaArrayDyV(int inicio, int fin, int[] array){
+		if(inicio == fin){
+			return array[inicio];
+		}else{
+			int mitad = (inicio+fin)/2;
+			int x = sumaArrayDyV(inicio, mitad, array);
+			int y = sumaArrayDyV(mitad+1, fin, array);
+			return x + y;
+		}
+	}
+}
