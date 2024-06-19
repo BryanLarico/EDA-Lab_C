d48055b04149f366f38b2b82fd575a67e0e15e40
BryanLarico <blaricor@unsa.edu.pe>
2024-06-18 23:10:18 -0500

    Clase Ejercicio, la clase Main, para poder hacer las pruebas necesarias

---
+++ Lab06/Ejercicio.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Ejercicio{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		Novela lib = new Novela("Biblia", "Jesus", "");
+		System.out.println(lib);
+	}
+}
