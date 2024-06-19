8ff5a59006a903fc20871ed2a9371a4ed2a55da4
BryanLarico <blaricor@unsa.edu.pe>
2024-06-18 23:33:14 -0500

    Clase Ejercicio, actualizacion ahora funciona todas las clases gracias a la verificacion

--- Lab06/Ejercicio.java
+++ Lab06/Ejercicio.java
@@ -2,7 +2,9 @@ import java.util.*;
 public class Ejercicio{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		Novela lib = new Novela("Biblia", "Jesus", "");
-		System.out.println(lib);
+		Novela novela = new Novela("Cien Años de Soledad", "Gabriel Garcia Marques", "Ciencia Ficcion");
+		LibroTextoUNSA libro = new LibroTextoUNSA("Fundamentos de la Programacion", "Marco Aedo", "FP2", "Produccion y Servicios");
+		System.out.println(novela + "\n");
+		System.out.println(libro);
 	}
 }
