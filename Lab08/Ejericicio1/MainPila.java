8ee3d35cb4074b6e250ddd1ee5a4c888cfa9cbf9
BryanLarico <blaricor@unsa.edu.pe>
2024-07-01 21:31:34 -0500

    Clase Main adecuada para la clase Pila

--- Lab08/Ejericicio1/MainPila.java
+++ Lab08/Ejericicio1/MainPila.java
@@ -1,10 +1,15 @@
 public class MainPila { 
 	public static void main(String[] args) { 
 		Pila pila = new Pila(); 
+		/*
 		pila.push("H"); 
 		pila.push("o"); 
 		pila.push("l");
 		pila.push("a");
-		pila.mostrar(); 
-	} 
+		pila.mostrar();
+		*/
+		pila = pila.palabraComoPila("Hola");
+		pila.mostrar();
+		System.out.println(pila);
+	}
 } 
