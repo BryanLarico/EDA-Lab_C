ba33d2f91e84362ba204a69962a03c8741b845d4
BryanLarico <blaricor@unsa.edu.pe>
2024-07-08 18:40:48 -0500

    Clase Main para hacer experimentos

---
+++ Lab09/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+	public static void main(String[] args) {
+		BinaryTree tree = new BinaryTree();
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+
+		System.out.println("Recorrido en preorden:");
+		tree.preOrder();
+
+		System.out.println("\nRecorrido en inorden:");
+		tree.inOrder();
+
+		System.out.println("\nRecorrido en postorden:");
+		tree.postOrder();
+	}
+}
+
+/* 
+				  1
+				/   \
+			 2			3
+		 /   \
+	  4     5
+
+*/
